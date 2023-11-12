8cba5ced551f3a77db0d42ad8e371bfffdf1535b
sergiodimep <67322463+sergiodimep@users.noreply.github.com>
2023-11-12 19:42:41 -0300

    Update teste.c
    corregido o código para versão final

--- teste.c
+++ teste.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+int somar(int a, int b){
+return a + b ;
+
 int main(){
-printf("ola mundo");
+int resultado;
+  resultado = somar(3,5);
+printf("O resultado de 3 + 5 = %d \n", resultado);
+return 0;
 }
